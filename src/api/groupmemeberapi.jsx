7b1c67b6d2d2714d4e3ee4d12ef87f34962e78da
Ayaan Naqvi <naqvinaved8@gmail.com>
2026-02-17 04:25:47 +0530

    changes in api

--- src/api/groupmemeberapi.jsx
+++ src/api/groupmemeberapi.jsx
@@ -1,21 +1,19 @@
-import React from "react";
 import { API_BASE } from "./apimain";
 
-export async function groupmembersapi(userId,groupId){
-    console.log("POST groupId:", groupId);
-console.log("POST userId:", userId);
-    const apires= await fetch(`https://expense-tracker-backend-jgkz.onrender.com/${userId}`,{
-method:"POST"
-    })
-   
-        return apires.json();   
-    
+export async function groupmembersapi(userId, groupId) {
+  const apires = await fetch(
+    `${API_BASE}/groups/${groupId}/users/${userId}`,
+    { method: "POST" }
+  );
+  if (!apires.ok) throw new Error("Add member failed");
+  return apires.json();
 }
 
-export async function getusers(groupId){
-    const apilistres= await fetch (`https://expense-tracker-backend-jgkz.onrender.com/groups/${groupId}/users`,{
-        method: "GET"
-    })
-   
-        return apilistres.json();
+export async function getusers(groupId) {
+  const apilistres = await fetch(
+    `${API_BASE}/groups/${groupId}/users`,
+    { method: "GET" }
+  );
+  if (!apilistres.ok) throw new Error("Get users failed");
+  return apilistres.json();
 }
